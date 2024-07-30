7cc5707178f28a7fc4120a402a20cc187fcb2953
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2024-07-30 22:22:56 +0530

    DONEEEEE

--- costingSheet.php
+++ costingSheet.php
@@ -38,7 +38,7 @@
                 <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px">Per Unit</th>
             </tr>
         </thead>
-        
+
         <tbody>
 
 
@@ -46,21 +46,140 @@
             $sql = "SELECT * FROM `product`";
             $result = mysqli_query($conn, $sql);
             $sno = 0;
-            while ($row = mysqli_fetch_assoc($result)) {                
-                $idProd = $row['_id'];        
+            while ($row = mysqli_fetch_assoc($result)) {
+                $idProd = $row['_id'];
+                $nameOfProd = $row['name'];
+                $sizeOfProd = $row['sizes'];
+                $typeOfProd = $row['type'];
+                // echo $idProd;
+
+
+                // Raw material search
+                $sqlRawMatSearch = "SELECT product_details.pid, product_details.raw_id, product_details.qty, raw_material.final_rate, raw_material._id from product_details JOIN raw_material on product_details.raw_id = raw_material._id WHERE product_details.pid = $idProd;";
+                $resultRawMatSearch = mysqli_query($conn, $sqlRawMatSearch);
+                $totalCostOfRawMat = 0;
+                while ($rowRawMat = mysqli_fetch_assoc($resultRawMatSearch)) {
+                    $quantity = $rowRawMat['qty'];
+                    $finalRate = $rowRawMat['final_rate'];
+                    $intermediateCost = $quantity * $finalRate;
+                    $totalCostOfRawMat += $intermediateCost;
+                }
+
+
+
+                // Packaging material search
+                $sqlPackMaterial = "SELECT packaging_material.pid, packaging_material.cover_box, packaging_material.label, packaging_material.jar, packaging_material.cartoon, packaging_material.cap, product._id from packaging_material JOIN product on packaging_material.pid = product._id WHERE product._id = $idProd;";
+                $resultPackMaterial = mysqli_query($conn, $sqlPackMaterial);
+                while ($rowPackMat = mysqli_fetch_assoc($resultPackMaterial)) {
+                    $coverBox = $rowPackMat['cover_box'];
+                    $jar = $rowPackMat['jar'];
+                    $label = $rowPackMat['label'];
+                    $cartoon = $rowPackMat['cartoon'];
+                    $cap = $rowPackMat['cap'];
+                    $totalCost = $coverBox + $jar + $label + $cartoon + $cap;
+                    $unitLot = 3; //??
+                    $totalCostLot = $totalCost * $unitLot;
+                }
+
+
+
+
+
+                // Labour search
+                $sqlEmpData = "SELECT SUM(gross_salary) from employee";
+                $runSqlEmpData = mysqli_query($conn, $sqlEmpData);
+                $row = mysqli_fetch_array($runSqlEmpData);
+                $totalEmpSal = $row[0];
+
+
+                $sqlCountEmp = "SELECT count(*) from employee";
+                $runSqlCountEmp = mysqli_query($conn, $sqlCountEmp);
+                $row = mysqli_fetch_array($runSqlCountEmp);
+                $totalEmp = $row[0];
+                // echo $totalEmp;
+
+                $middleCal = $totalEmpSal / $totalEmp;
+
+                $final = $middleCal / 26;
+
+
+                $sqlLabourData = "SELECT * FROM `labour_in_packing` where pid = $idProd";
+                $resultLabourData = mysqli_query($conn, $sqlLabourData);
+                while ($rowLabour = mysqli_fetch_assoc($resultLabourData)) {
+                    $empCostDay = $final;
+                    $costFilling = ($rowLabour['unit_prds_per_ghan'] / $rowLabour['filling']) * $empCostDay;
+                    $costSealing = ($rowLabour['unit_prds_per_ghan'] / $rowLabour['sealing']) * $empCostDay;
+                    $costPacking = ($rowLabour['unit_prds_per_ghan'] / $rowLabour['packing']) * $empCostDay;
+                    $totalCostLabour = $costFilling + $costPacking + $costSealing;
+                }
+
+
+
+
+
+                // lot ghan search
+                $sqlLot = "SELECT * FROM `lot_ghan` where pid = $idProd";
+                $resultData = mysqli_query($conn, $sqlLot);
+                while ($rowLot = mysqli_fetch_assoc($resultData)) {
+                    $lotData = $rowLot['pks_output'];
+                }
+
+                
+                $overheadTotal = ($totalCostOfRawMat + $totalCostLot + $totalCostLabour)*6/94;
+                $primeCost = $totalCostOfRawMat + $totalCostLot + $totalCostLabour + $overheadTotal;
+                $lotDataLink = $lotData;
+                $primeCostPerUnit = $primeCost/$lotDataLink;
+                $freight = $primeCostPerUnit*0.015;
+                $costOfSale = $primeCostPerUnit + $freight;
+                $wholeSalePrice = 10; //???
+                $profitPerUnit = $wholeSalePrice - $primeCostPerUnit;
+  
                 $sno += 1;
                 echo "
                     <tr class='text-gray-700 dark:text-gray-100'>
                         <td class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>" . $sno . "</td>
                             
-                        <th class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left'><a href ='viewProd.php?id=".$idProd."'>" . $row['name'] . " - " . $row['sizes'] . "
+                        <th class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left'><a href ='viewProd.php?id=" . $idProd . "'>" . $nameOfProd . " - " . $sizeOfProd . "
                         </a>
                         </th>
                         <td class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
-                            " . $row['type'] . "
+                            " . $typeOfProd . "
                         </td>
                         <td class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
                             " . $totalCostOfRawMat . "
+                        </td>
+                        <td class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+                            " . $totalCostLot . "
+                        </td>
+                        <td class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+                            " . $totalCostLabour . "
+                        </td>
+                        <td class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+                            EMPTY
+                        </td>
+                        <td class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+                            " . $overheadTotal . "
+                        </td>
+                        <td class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+                            " . $primeCost . "
+                        </td>
+                        <td class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+                            " . $lotDataLink . "
+                        </td>
+                        <td class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+                            " . $primeCostPerUnit . "
+                        </td>
+                        <td class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+                            " . $freight . "
+                        </td>
+                        <td class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+                            " . $costOfSale . "
+                        </td>
+                        <td class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+                            EMPTY
+                        </td>
+                        <td class='border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4'>
+                            " . $profitPerUnit . "
                         </td>
                     </tr>
                 ";
