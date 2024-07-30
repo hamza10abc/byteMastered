c2fa2cd06b3eee23c165f861e5c13336d18a65ac
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2024-07-30 22:51:20 +0530

    Download Updated

--- dashboard.php
+++ dashboard.php
@@ -84,11 +84,22 @@ $result = mysqli_query($conn, $sql);
 
   <div class="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-6">
     <div class="bg-white border border-gray-100 shadow-md shadow-black/5 p-6 rounded-md lg:col-span-3">
-      <div class="flex justify-between mb-4 items-start">
-        <div class="font-medium">Costing Sheet</div>
-
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
+        <div class="flex justify-between mb-4 items-start">
+          <div class="font-medium">Costing Sheet</div>
+        </div>
+        <div class="p-4 col-start-4">
+          <a href="#">
+            <button class="bg-blue-300 hover:bg-blue-400 text-blue-800 font-bold py-2 px-4 rounded inline-flex items-center">
+              <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+              </svg>
+              <span>Download</span>
+            </button>
+          </a>
+        </div>
       </div>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
+      <!-- <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
         <div class="rounded-md border border-dashed border-gray-200 p-4">
           <button class="bg-blue-300 hover:bg-blue-400 text-blue-800 font-bold py-2 px-4 rounded inline-flex items-center">
             <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -116,7 +127,7 @@ $result = mysqli_query($conn, $sql);
           </button>
           <span class="text-gray-400 text-sm ml-2">Custom download</span>
         </div>
-      </div>
+      </div> -->
       <div>
         <?php include 'costingSheet.php' ?>
         <!-- <table class="items-center w-full bg-transparent border-collapse">
